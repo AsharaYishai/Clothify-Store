cd518d3714dac4795414e2fde60b0696c7094cc3
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-06-20 14:09:06 +0530

    Employee Registration Form

---
+++ src/main/java/org/clothifys/controller/EmployeeRegistrationForm.java
@@ -0,0 +1,36 @@
+package org.clothifys.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class EmployeeRegistrationForm {
+    public DatePicker dateDob;
+    public TextField txtContactNo;
+    public TextField txtEmail;
+    public TextField txtCustomerId;
+    public TextField txtAddress;
+    public TextField txtNic;
+    public TextField txtName;
+    public ComboBox cmbTitle;
+    public TableView tblEmployee;
+    public TableColumn colId;
+    public TableColumn colTitel;
+    public TableColumn colName;
+    public TableColumn colNic;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colDob;
+    public TableColumn colAddress;
+
+    public void btnAddEmployeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
+}
