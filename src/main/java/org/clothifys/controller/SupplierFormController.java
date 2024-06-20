124fdf825bf2e55fdd589a864011c2ee7f5f0a32
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-06-20 14:59:57 +0530

    Suppliers Form

---
+++ src/main/java/org/clothifys/controller/SupplierFormController.java
@@ -0,0 +1,40 @@
+package org.clothifys.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class SupplierFormController {
+    public TextField txtContactNo;
+    public TextField txtEmail;
+    public TextField txtSupplierId;
+    public TextField txtAddress;
+    public TextField txtCompany;
+    public TextField txtName;
+    public ComboBox cmbTitle;
+    public TableView tblSupplier;
+    public TableColumn colSupplier;
+    public TableColumn colTitle;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colCompany;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colItemCode;
+    public TableColumn colDescription;
+    public TableColumn colQty;
+
+    public void btnAddSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
+}
